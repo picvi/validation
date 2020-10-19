36b7ebbcd768e0c25f247eb080869fa26ffbcd1e
picvi <viki.galaburda@gmail.com>
2020-10-19 16:41:51 +0300

    keys validation

--- validation/validateObject.js
+++ validation/validateObject.js
@@ -1,4 +1,21 @@
-import { validateField } from './validateField.js';
+function validateField(object, key, schemeField) {
+  const errorList = [];
+
+  const userKeyValue = object[key];
+
+  schemeField.forEach((field) => {
+    const args = [userKeyValue, ...field.validator.param];
+
+    if (!field.validator.rule.apply(null, args)) {
+      errorList.push({
+        key,
+        message: field.validationMessage,
+      });
+    }
+  });
+
+  return errorList;
+}
 
 export function validateObject(object, scheme) {
   const errorList = [];
