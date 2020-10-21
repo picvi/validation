f7288840a1829bfa3f2953f01c9979d2538f05be
picvi <viki.galaburda@gmail.com>
2020-10-21 10:21:23 +0300

    changed key validation

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
 import { errorsDisplay } from './validation/errorsDisplay.js';
 import { validateObject } from './validation/validateObject.js';
 import { user, userScheme } from './Scheme/userScheme.js';
-import { keysValidation } from './validation/keysValidation.js';
+import { checkExtraKeys, checkLackOfKeys } from './validation/keysValidation.js';
 
 const results = validateObject(user, userScheme);
-const keysResult = keysValidation(user, userScheme);
+const extraKeys = checkExtraKeys(user, userScheme);
+const lackOfKeys = checkLackOfKeys(user, userScheme);
 
-if (keysResult.length !== 0) {
-  console.log('Keys are not equal!');
-} else {
-  console.log('Keys are equal!');
+if (extraKeys.length !== 0) {
+  console.log('You have too many questions');
+}
+
+if (lackOfKeys.length !== 0) {
+  console.log('Not enough questions');
 }
 
 if (results.length !== 0) {
--- validation/keysValidation.js
+++ validation/keysValidation.js
@@ -1,13 +1,30 @@
-export function keysValidation(object, scheme) {
+/* eslint-disable array-callback-return */
+function checkExtraKeys(object, scheme) {
   const objKeys = Object.keys(object);
   const schemeKeys = Object.keys(scheme);
   const totalKeys = objKeys.concat(schemeKeys);
   const missingKeys = [];
 
   totalKeys.filter((key) => {
-    if (!objKeys.includes(key) || !schemeKeys.includes(key)) {
+    if (objKeys.includes(key) && !schemeKeys.includes(key)) {
       missingKeys.push(key);
     }
   });
   return missingKeys;
 }
+
+function checkLackOfKeys(object, scheme) {
+  const objKeys = Object.keys(object);
+  const schemeKeys = Object.keys(scheme);
+  const totalKeys = objKeys.concat(schemeKeys);
+  const missingKeys = [];
+
+  totalKeys.filter((key) => {
+    if (!objKeys.includes(key) && schemeKeys.includes(key)) {
+      missingKeys.push(key);
+    }
+  });
+  return missingKeys;
+}
+
+export {checkExtraKeys, checkLackOfKeys}
